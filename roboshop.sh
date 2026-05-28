8752aad7424fb29d891fda54b101d493626fa7bf
PrudhviMV <prudhvimenta@gmail.com>
2026-05-28 07:56:00 +0530

    server automation

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=mongodb}]" --query 'Instances[0].InstanceId' --output text)
 
-    if [ $instance -ne "frontend" ]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
         R53_record="$instance.$DOMAIN"
     else
