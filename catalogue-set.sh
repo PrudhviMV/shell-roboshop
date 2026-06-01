898d8cc3db6ff346170755796f1e30aab032198a
PrudhviMV <prudhvimenta@gmail.com>
2026-06-01 10:13:46 +0530

    catalogue set

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -87,7 +87,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 #VALIDATE $? "Installing Mongodb"
 
-INDEX=$(mongosh $Mongo_host --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.prudhvii.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     #VALIDATE $? "Load catalogue products"
