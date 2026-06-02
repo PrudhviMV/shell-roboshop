6c1b47436d52a28293839f1180a907af261c0c6d
PrudhviMV <prudhvimenta@gmail.com>
2026-06-02 19:18:32 +0530

    Adding catalogue conf and shell scriptt

--- catalogue.sh
+++ catalogue.sh
@@ -82,7 +82,7 @@ VALIDATE $? "Adding mongo repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb"
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.prudhvii.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
